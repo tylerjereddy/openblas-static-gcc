a7a207ef29079e16003c016356501d98b7946229
Tyler Reddy <tyler.je.reddy@gmail.com>
2019-04-02 17:24:41 +0000

    Simplified POWER8 build -- it now works in this simple form

--- power8/build_openblas.sh
+++ power8/build_openblas.sh
@@ -12,19 +12,13 @@ git clean -xfd
 rm -rf $OPENBLAS_ROOT/$BUILD_BITS
 
 # set flags
-march="powerpc"
 extra="-fno-asynchronous-unwind-tables"
 # for static gcc inclusion see:
 # https://github.com/xianyi/OpenBLAS/issues/1172
 # also: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=46539
-cflags="-O2 -march=$march -mtune=generic $extra -static-libgcc -static-libgfortran"
+cflags="-O2 -mcpu=power8 -mtune=power8 $extra -static-libgcc -static-libgfortran"
 fflags="$cflags -frecursive -ffpe-summary=invalid,zero"
 
 # Build OpenBLAS
-make BINARY=$BUILD_BITS DYNAMIC_ARCH=1 USE_THREAD=1 USE_OPENMP=0 \
-     NUM_THREADS=64 NO_WARMUP=1 NO_AFFINITY=1 CONSISTENT_FPCSR=1 \
-     BUILD_LAPACK_DEPRECATED=1 \
-     COMMON_OPT="$cflags" \
-     FCOMMON_OPT="$fflags" \
-     MAX_STACK_ALLOC=2048
+make TARGET=POWER8
 make PREFIX=$OPENBLAS_ROOT/$BUILD_BITS install
